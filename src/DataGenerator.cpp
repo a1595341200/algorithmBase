68ca06d1085e414f23c1fee93697c85bdbdf8f63
yao.xie <1595341200@qq.com>
2024-03-21 09:38:26 +0800

    修复编译错误

--- src/DataGenerator.cpp
+++ src/DataGenerator.cpp
@@ -1,4 +1,15 @@
+/*
+ * @Author: yao.xie 1595341200@qq.com
+ * @Date: 2024-03-21 09:32:26
+ * @LastEditors: yao.xie 1595341200@qq.com
+ * @LastEditTime: 2024-03-21 09:36:02
+ * @FilePath: /cplusplus/submodule/algorithmBase/src/DataGenerator.cpp
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
 #include "DataGenerator.h"
+#include <algorithm>
 namespace algorithmBase {
 
 double generateNormalDistributionData(double mean, double stdDev) {
